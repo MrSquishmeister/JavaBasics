80e4aeba943dd9fbfdbe874b3c913b7a114f593a
psyal5 <psyal5@nottingham.ac.uk>
2023-01-08 04:50:42 +0000

    bank database update feat : deposit & withdraw options that update balance in database

--- src/main/java/com/lee/javabasics/controller/BankController.java
+++ src/main/java/com/lee/javabasics/controller/BankController.java
@@ -4,6 +4,7 @@ import com.lee.javabasics.SceneSwitch;
 import com.lee.javabasics.model.BankModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -16,20 +17,38 @@ public class BankController {
     @FXML
     private AnchorPane bankAnchorPane;
     @FXML
+    Pane loginPane;
+    @FXML
+    Pane accountPane;
+    @FXML
     private TextField userIDTextField;
     @FXML
     private TextField userPinTextField;
     @FXML
+    private TextField inputTextField;
+    @FXML
+    private Label welcomeLabel;
+    @FXML
     private Label promptLabel;
+    @FXML
+    private Label balanceLabel;
     private BankModel BankModel = new BankModel();
+    private int userID, userPIN, userBalance;
+
+    void AccountSuccess(){
+        loginPane.setVisible(false);
+        accountPane.setVisible(true);
+        welcomeLabel.setText("Welcome to the Bank userID " + userID);
+        balanceLabel.setText("Balance: £" + userBalance);
+    }
 
     @FXML
-    void loginButtonClick(ActionEvent event) {
-
+    void onloginButtonClick() {
         try{
-            int userID = Integer.parseInt(userIDTextField.getText());
-            int userPIN = Integer.parseInt(userPinTextField.getText());
-            BankModel.Login(event, userID, userPIN);
+            userID = Integer.parseInt(userIDTextField.getText());
+            userPIN = Integer.parseInt(userPinTextField.getText());
+            userBalance = BankModel.Login(userID, userPIN);
+            AccountSuccess();
         }
         catch (NumberFormatException e){
             promptLabel.setText("Provide credentials to login");
@@ -39,7 +58,21 @@ public class BankController {
     }
 
     @FXML
-    void BackButtonClick() throws IOException {
+    void onButtonClick(ActionEvent event){
+        Button button = (Button) event.getSource();
+        String operatorString = button.getText();
+        try{
+            int userInput = Integer.parseInt(inputTextField.getText());
+            userBalance = BankModel.Deposit(userInput, operatorString);
+            balanceLabel.setText("Balance: £" + userBalance);
+        }catch (NumberFormatException e){
+            System.out.println("NOT INT");
+        }
+    }
+
+
+    @FXML
+    void onBackButtonClick() throws IOException {
         new SceneSwitch(bankAnchorPane, "view/intro-view.fxml");
     }
 
--- src/main/java/com/lee/javabasics/model/BankModel.java
+++ src/main/java/com/lee/javabasics/model/BankModel.java
@@ -1,68 +1,36 @@
 package com.lee.javabasics.model;
 
-import com.lee.javabasics.JavaBasicsApplication;
-import com.lee.javabasics.controller.AccountController;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.sql.*;
 
 public class BankModel {
-    public void ChangeScene(ActionEvent event, String fxml, int userID, int userBalance){
-        Parent root = null;
+    private int userID, userBalance;
 
-        if(userID != 0 && userBalance != 0){
-            try{
-                FXMLLoader loader = new FXMLLoader (JavaBasicsApplication.class.getResource(fxml));
-                root = loader.load();
-                AccountController accountController = loader.getController();
-                accountController.UserInformation(userID, userBalance);
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-        }
-        else {
-            try{
-                root = FXMLLoader.load(JavaBasicsApplication.class.getResource(fxml));
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("Java Basics");
-        stage.setScene(new Scene(root, 800, 400));
-        stage.show();
-    }
-
-    public void Login(ActionEvent event, int userID, int userPIN) throws SQLException {
+    public int Login(int inputuserID, int userPIN) throws SQLException {
+        userID = inputuserID;
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
-        try{
+        try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankDB", "root", "rootpassword");
             preparedStatement = connection.prepareStatement("SELECT userPIN, userBalance FROM bankDB.users WHERE userID = ?");
             preparedStatement.setInt(1, userID);
             resultSet = preparedStatement.executeQuery();
 
-            if(!resultSet.isBeforeFirst()){
+            if (!resultSet.isBeforeFirst()) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setContentText("Provided userID not found");
                 alert.show();
-            }else{
-                while (resultSet.next()){
+            } else {
+                while (resultSet.next()) {
                     int retrievedUserPIN = resultSet.getInt("userPIN");
-                    int retrievedBalance = resultSet.getInt("userBalance");
-                    if (retrievedUserPIN == userPIN){
+                    userBalance = resultSet.getInt("userBalance");
+                    if (retrievedUserPIN == userPIN) {
                         System.out.println("userPIN MATCHES");
-                        ChangeScene(event, "view/account-view.fxml", userID, retrievedBalance);
-                    }else{
+                        return userBalance;
+                    } else {
                         Alert alert = new Alert(Alert.AlertType.ERROR);
                         alert.setContentText("Provided userPin incorrect");
                         alert.show();
@@ -70,25 +38,24 @@ public class BankModel {
                 }
             }
 
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if(resultSet != null) {
+            if (resultSet != null) {
                 try {
                     resultSet.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
-            if(preparedStatement != null) {
+            if (preparedStatement != null) {
                 try {
                     preparedStatement.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
-            if(connection != null) {
+            if (connection != null) {
                 try {
                     connection.close();
                 } catch (SQLException e) {
@@ -96,5 +63,44 @@ public class BankModel {
                 }
             }
         }
+        return 0;
     }
+
+    public int Deposit(int userInput, String operator) {
+        if(operator.equals("Deposit")){
+            userBalance = userBalance + userInput;
+        }else if (operator.equals("Withdraw")){
+            userBalance = userBalance - userInput;
+        }
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankDB", "root", "rootpassword");
+            preparedStatement = connection.prepareStatement("UPDATE bankDB.users SET userBalance = ?  WHERE userID = ?");
+            preparedStatement.setInt(1, userBalance);
+            preparedStatement.setInt(2, userID);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return userBalance;
+    }
+
 }
